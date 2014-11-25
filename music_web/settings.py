32069417c4a2d19a5e47538d117755aab22d54db
Jay <smile665@gmail.com>
2014-11-25 15:23:33 +0800

    update setttings & urls

--- music_web/settings.py
+++ music_web/settings.py
@@ -30,12 +30,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'music',
+    'api',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +54,12 @@ WSGI_APPLICATION = 'music_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jay_data',
+        'USER': 'jay',
+        'PASSWORD': 'jay',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
     }
 }
 
