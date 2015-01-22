cfd7f14fb86a3b29bdccbfee1e9cea15401ea450
Jay <smile665@gmail.com>
2015-01-22 17:01:56 +0800

    add add_user POST API

--- api/user_urls.py
+++ api/user_urls.py
@@ -6,5 +6,6 @@ Created on Jan 15, 2015
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
+    url('^add_user', 'api.user_views.add_user'),
     url('^get_user', 'api.user_views.get_user_by_openid'),
 )
--- api/user_views.py
+++ api/user_views.py
@@ -7,7 +7,7 @@ Created on Jan 15, 2015
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.request import Request
 from lib.response import JSONResponse
-from user.qq_user import get_user
+from user.qq_user import get_user, add_update_user
 
 
 @csrf_exempt
@@ -22,4 +22,22 @@ def get_user_by_openid(request):
         return JSONResponse(data=data, status=200)
     else:
         return JSONResponse({'error': 'It only support HTTP GET method.'},
+            status=200)
+
+
+@csrf_exempt
+def add_user(request):
+    '''
+    @summary: add (update) a user  from QQ login or .
+    '''
+    if request.method == 'POST':
+        req = Request(request)
+        data = req.DATA
+        force = data.get('force', default=False)
+        if force:
+            force = True
+        openid = add_update_user(data, force=force)
+        return JSONResponse(data={'openid': openid}, status=200)
+    else:
+        return JSONResponse({'error': 'It only support HTTP POST method.'},
             status=200)
