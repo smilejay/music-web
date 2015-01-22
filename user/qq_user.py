daf4e9b6f98ce371ba2788050d213792f10eaf96
Jay <smile665@gmail.com>
2015-01-22 17:21:11 +0800

    add add_update_user()

--- user/qq_user.py
+++ user/qq_user.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Created on Jan 15, 2015
 
@@ -38,7 +39,34 @@ def get_user(openid):
         pass
     return ret
 
+
+def add_update_user(data, force=False):
+    '''
+    @summary: add or update a user's profile.
+    @param data: a user's dict data.
+    @param force: if True, update all the profile from data;
+                  else only update 'figureurl' field.
+    @return: the openid or None.
+    '''
+    ret = None
+    if not 'openid' in data:
+        pass
+    else:
+        user = get_user(data['openid'])
+        if user and not force:
+            if 'figureurl' in data:
+                user['figureurl'] = data['figureurl']
+            add_update(user)
+        else:
+            add_update(data)
+        ret = data['openid']
+    return ret
+
+
 if __name__ == '__main__':
     django.setup()
     openid = '123abc'
     print get_user(openid)
+    data = {'openid': '123abc', 'nickname': '笑遍世界', 'city': '杭州',
+            'figureurl': 'http://smilejay.b0.upaiyun.com/wp-content/uploads/2013/09/kvm_principals_and_practices1.jpg'}
+    add_update_user(data, force=False)
