76369985d18fb2c88d0efd83f5c9efb74aac2842
Jay <smile665@gmail.com>
2015-01-26 23:58:17 +0800

    add age field to User model

--- user/models.py
+++ user/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
     openid = models.CharField(max_length=64, db_index=True)
     nickname = models.CharField(max_length=64, blank=True)
     gender = models.CharField(max_length=4, blank=True)
+    age = models.SmallIntegerField(blank=True)
     figureurl = models.CharField(max_length=255, blank=True)
     city = models.CharField(max_length=32, blank=True)
     create_time = models.DateTimeField(auto_now_add=True)
